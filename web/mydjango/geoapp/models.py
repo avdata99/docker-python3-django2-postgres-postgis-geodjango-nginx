4c6884938601fac1a37c94e2d1451713413b16c7
Andres <andres@data99.com.ar>
2018-03-25 21:55:44 -0300

    detalles para que tambien funcionen imágenes y upload

--- web/mydjango/geoapp/models.py
+++ web/mydjango/geoapp/models.py
@@ -5,6 +5,7 @@ from django.contrib.gis.db import models as models_geo
 class GeoAlgo(models.Model):
     nombre = models.CharField(max_length=90)
     poligono = models_geo.PolygonField()
+    foto = models.ImageField(null=True, blank=True, upload_to='fotos')
     
     def __str__(self):
         return self.nombre
--- web/mydjango/mydjango/settings.py
+++ web/mydjango/mydjango/settings.py
@@ -146,7 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = '/media/'
-STATIC_ROOT = 'media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
